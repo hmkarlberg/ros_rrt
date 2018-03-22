7c58539ca9b5dc6c7a79a252ddd97a5acc2180f3
Hampus Karlberg <hkarlb@kth.se>
2018-03-22 20:34:52 +0100

    .

--- arm.py
+++ arm.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python
+import numpy as np
+
+class LFCone:
+
+    def __init__(self, dimensionality):
+        self.dim = dimensionality
+        self.force = np.zeros((1, self.dim + 1))
+
+
+
+class Arm:
+
+    def __init__(self):
+
+
+    class Joint:
+
+        def __init__(self):
+
+
+    class Gripper:
+
+        def __init__(self):
+
+            class GripModel:
+
+                def __init__(self, contacts):
